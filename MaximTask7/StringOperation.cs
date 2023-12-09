4e50e027f592592f403993a28bc3d94ad93836ca
User <misimov@list.ru>
2023-12-09 18:18:57 +0500

    Изменил класс StringOperation

--- MaximTask7/StringOperation.cs
+++ MaximTask7/StringOperation.cs
@@ -2,12 +2,6 @@ namespace MaximTask7
 {
     public class StringOperation
     {
-        public DateOnly Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
     }
 }
